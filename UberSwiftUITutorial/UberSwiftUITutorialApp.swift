88dfbee7540cd75267d3c0076b744246cd7c9cfe
Tiziano Cialfi <tiziano.cialfi@soft.it>
2023-06-09 09:34:01 +0200

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Tiziano Cialfi on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
